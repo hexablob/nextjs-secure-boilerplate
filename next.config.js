1af73c2673d5eec1556d246b1bb7c57ef46de655
Jean-Vincent QUILICHINI <jv.quilichini@gmail.com>
2023-05-05 09:11:22 +0200

    feat: added basic security headers & csp to next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,29 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  headers() {
+    return [{ source: '/(.*)', headers: securityHeaders }];
+  },
+};
 
-module.exports = nextConfig
+const ContentSecurityPolicy = `
+  default-src 'self' vercel.live;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`.replace(/\n/g, '');
+
+const securityHeaders = [
+  { key: 'Content-Security-Policy', value: ContentSecurityPolicy },
+  { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+  { key: 'X-Frame-Options', value: 'DENY' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'X-DNS-Prefetch-Control', value: 'on' },
+  { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
+  { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+];
+
+module.exports = nextConfig;
